58805bb037486c46164ae78e5576483177813671
likai <1195419506@qq.com>
2021-11-29 22:11:47 +0800

    fix: 完美解决确认后的截图精度不准问题
    生成裁剪框时对右上、右下、左上拖拽时生成的坐标做兼容处理，解决负数问题
    删除生成图片时的边框兼容处理以及坐标负数处理

--- src/lib/common-methords/GetCanvasImgData.ts
+++ src/lib/common-methords/GetCanvasImgData.ts
@@ -11,88 +11,20 @@ export function getCanvasImgData(isSave: boolean) {
   // 获取裁剪区域位置信息
   const { startX, startY, width, height } = data.getCutOutBoxPosition();
   let base64 = "";
-  // 保存图片,需要减去八个点的大小
   if (screenShortCanvas) {
     if (isSave) {
       // 将canvas转为图片
-      if (width > 0 && height > 0) {
-        // 考虑左上往右下拉区域的情况
-        saveCanvasToImage(
-          screenShortCanvas,
-          startX + data.getBorderSize() / 2,
-          startY + data.getBorderSize() / 2,
-          width - data.getBorderSize(),
-          height - data.getBorderSize()
-        );
-      } else if (width < 0 && height < 0) {
-        // 考虑右下往左上拉区域的情况
-        saveCanvasToImage(
-          screenShortCanvas,
-          startX + width + data.getBorderSize() / 2,
-          startY + height + data.getBorderSize() / 2,
-          Math.abs(width) - data.getBorderSize(),
-          Math.abs(height) - data.getBorderSize()
-        );
-      } else if (width > 0 && height < 0) {
-        // 考虑左下往右上拉区域的情况
-        saveCanvasToImage(
-          screenShortCanvas,
-          startX + data.getBorderSize() / 2,
-          startY + height + data.getBorderSize() / 2,
-          width - data.getBorderSize(),
-          Math.abs(height) - data.getBorderSize()
-        );
-      } else if (width < 0 && height > 0) {
-        // 考虑右上往左下拉区域的情况
-        saveCanvasToImage(
-          screenShortCanvas,
-          startX + width + data.getBorderSize() / 2,
-          startY + data.getBorderSize() / 2,
-          Math.abs(width) - data.getBorderSize(),
-          height - data.getBorderSize()
-        );
-      }
+      saveCanvasToImage(screenShortCanvas, startX, startY, width, height);
     } else {
       // 将canvas转为base64
-      if (width > 0 && height > 0) {
-        // 考虑左上往右下拉区域的情况
-        base64 = saveCanvasToBase64(
-          screenShortCanvas,
-          startX + data.getBorderSize() / 2,
-          startY + data.getBorderSize() / 2,
-          width - data.getBorderSize(),
-          height - data.getBorderSize()
-        );
-      } else if (width < 0 && height < 0) {
-        // 考虑右下往左上拉区域的情况
-        base64 = saveCanvasToBase64(
-          screenShortCanvas,
-          startX + width + data.getBorderSize() / 2,
-          startY + height + data.getBorderSize() / 2,
-          Math.abs(width) - data.getBorderSize(),
-          Math.abs(height) - data.getBorderSize()
-        );
-      } else if (width > 0 && height < 0) {
-        // 考虑左下往右上拉区域的情况
-        base64 = saveCanvasToBase64(
-          screenShortCanvas,
-          startX + data.getBorderSize() / 2,
-          startY + height + data.getBorderSize() / 2,
-          width - data.getBorderSize(),
-          Math.abs(height) - data.getBorderSize()
-        );
-      } else if (width < 0 && height > 0) {
-        // 考虑右上往左下拉区域的情况
-        base64 = saveCanvasToBase64(
-          screenShortCanvas,
-          startX + width + data.getBorderSize() / 2,
-          startY + data.getBorderSize() / 2,
-          Math.abs(width) - data.getBorderSize(),
-          height - data.getBorderSize()
-        );
-      }
+      base64 = saveCanvasToBase64(
+        screenShortCanvas,
+        startX,
+        startY,
+        width,
+        height
+      );
     }
   }
-  // 销毁组件
   return base64;
 }
