ea48ff1b3f99a7bf90b488d2eaa11007705c5aee
hqh <642134542@qq.com>
2021-12-02 13:11:16 +0800

    feat: html2canvas增加对在线图片资源的绘制

--- src/main.ts
+++ src/main.ts
@@ -194,8 +194,41 @@ export default class ScreenShort {
     this.data.showScreenShortPanel();
     if (!plugInParameters.getWebRtcStatus()) {
       // html2canvas截屏
-      html2canvas(document.body, {})
-        .then(canvas => {
+      html2canvas(document.body, {
+        onclone(html) {
+          const promises: any[] = [];
+          const imageNodes = html.querySelectorAll("img");
+          imageNodes.forEach(element => {
+            const href = element.getAttribute("src");
+            if (!href) return;
+            if (href && href.startsWith("base64")) return;
+            const promise = new Promise((resolve, reject) => {
+              const img = new Image();
+              img.crossOrigin = "anonymous";
+              img.src = `${href}&time=${+new Date().getTime()}`;
+              img.onload = function() {
+                const width = element.width;
+                const height = element.height;
+                const canvas = document.createElement("canvas");
+                canvas.width = width;
+                canvas.height = height;
+                const ctx: any = canvas.getContext("2d");
+                ctx.drawImage(img, 0, 0, width, height);
+                const base64 = canvas?.toDataURL();
+                element.setAttribute("src", base64);
+                resolve();
+              };
+              img.onerror = reject;
+              if (href !== null) {
+                img.src = href;
+              }
+            });
+            promises.push(promise);
+          });
+          return Promise.all(promises);
+        }
+      })
+        .then((canvas: HTMLCanvasElement) => {
           // 装载截图的dom为null则退出
           if (this.screenShortController == null) return;
 
