3a2c734fe9050bc274eb1234439f8618a5b08ba7
likai <1195419506@qq.com>
2022-10-17 22:56:24 +0800

    fix: 修复高分屏下保存图片比例错误问题

--- src/lib/common-methords/SaveCanvasToImage.ts
+++ src/lib/common-methords/SaveCanvasToImage.ts
@@ -1,3 +1,5 @@
+import { getCanvas2dCtx } from "@/lib/common-methords/CanvasPatch";
+
 export function saveCanvasToImage(
   context: CanvasRenderingContext2D,
   startX: number,
@@ -5,14 +7,20 @@ export function saveCanvasToImage(
   width: number,
   height: number
 ) {
+  // 获取设备像素比
+  const dpr = window.devicePixelRatio || 1;
   // 获取裁剪框区域图片信息
-  const img = context.getImageData(startX, startY, width, height);
+  // 获取裁剪框区域图片信息
+  const img = context.getImageData(
+    startX * dpr,
+    startY * dpr,
+    width * dpr,
+    height * dpr
+  );
   // 创建canvas标签，用于存放裁剪区域的图片
   const canvas = document.createElement("canvas");
-  canvas.width = width;
-  canvas.height = height;
   // 获取裁剪框区域画布
-  const imgContext = canvas.getContext("2d");
+  const imgContext = getCanvas2dCtx(canvas, width, height);
   if (imgContext) {
     // 将图片放进裁剪框内
     imgContext.putImageData(img, 0, 0);
