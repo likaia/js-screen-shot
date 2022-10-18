d150966e2cf3ed926b8b0c5954a3e56b1b45b58a
likai <1195419506@qq.com>
2022-10-18 21:17:17 +0800

    fix: 优化工具栏撤销函数代码逻辑

--- src/lib/common-methords/TakeOutHistory.ts
+++ src/lib/common-methords/TakeOutHistory.ts
@@ -5,22 +5,21 @@ import InitData from "@/lib/main-entrance/InitData";
 
 export function takeOutHistory() {
   const data = new InitData();
-  const lastImageData = data.popHistory();
-  const screenShotCanvas = data.getScreenShotContainer()?.getContext("2d");
-  if (screenShotCanvas != null && lastImageData) {
-    const context = screenShotCanvas;
-    if (data.getUndoClickNum() == 0 && data.getHistory().length > 0) {
-      // 首次取出需要取两条历史记录
-      const firstPopImageData = data.popHistory() as Record<string, any>;
-      context.putImageData(firstPopImageData["data"], 0, 0);
-    } else {
-      context.putImageData(lastImageData["data"], 0, 0);
+  data.popHistory();
+  const screenShortCanvas = data.getScreenShotContainer()?.getContext("2d");
+  if (screenShortCanvas != null) {
+    if (data.getHistory().length > 0) {
+      screenShortCanvas.putImageData(
+        data.getHistory()[data.getHistory().length - 1]["data"],
+        0,
+        0
+      );
     }
   }
 
   data.setUndoClickNum(data.getUndoClickNum() + 1);
   // 历史记录已取完，禁用撤回按钮点击
-  if (data.getHistory().length <= 0) {
+  if (data.getHistory().length - 1 <= 0) {
     data.setUndoClickNum(0);
     data.setUndoStatus(false);
   }
