78ada836aae7d4220f9bb64beef524e0fc4bded4
likai <1195419506@qq.com>
2023-04-12 20:39:50 +0800

    docs: 调整jest的配置

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,8 @@ module.exports = {
   verbose: true,
   // 是否显示覆盖率报告
   collectCoverage: true,
-  // 告诉 jest 哪些文件需要经过单元测试测试
-  collectCoverageFrom: ["/tests/*.test.ts", "src/lib/**/*.ts"],
+  // 告诉 jest 哪些文件需要经过单元测试
+  collectCoverageFrom: ["src/lib/**/*.ts", "src/*.ts"],
   testMatch: ["**/tests/**/*.test.ts", "**/tests/**/*.spec.ts"],
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
   coverageThreshold: {
@@ -23,5 +23,5 @@ module.exports = {
     "^@/(.*)$": "<rootDir>/src/$1"
   },
   // 运行时的环境
-  testEnvironment: "jsdom"
+  testEnvironment: "jest-environment-jsdom"
 };
